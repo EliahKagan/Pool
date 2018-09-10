2eb14e265ceb82888d2ea9f9940e869b8e22f8f2
Eliah Kagan <degeneracypressure@gmail.com>
2018-09-09 20:18:21 -0400

    Pool class skeleton

---
+++ Pool.h
@@ -0,0 +1,9 @@
+#ifndef POOL_POOL_H_
+#define POOL_POOL_H_
+
+template<typename T>
+class Pool {
+
+};
+
+#endif // ! POOL_POOL_H_
--- test.cpp
+++ test.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include "Pool.h"
 
-int main(int, char**) {
-    std::cout << "Hello, world!\n";
+int main()
+{
+    Pool<int> pool;
 }
