d4ee0d29de38120497c1b6b54b3725799966eeb5
Eliah Kagan <degeneracypressure@gmail.com>
2018-09-10 04:52:41 -0400

    finished initial test

--- test.cpp
+++ test.cpp
@@ -8,5 +8,8 @@ int main()
 
     std::vector<int*> a;
     for (auto i = 5; i != 0; --i)
-        a.push_back(pool());
+        a.push_back(pool(i));
+
+    for (const auto x : a)
+        std::cout << static_cast<const void*>(x) << ": " << *x << '\n';
 }
