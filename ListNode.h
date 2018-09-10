cf1ebbbd3943a62a8ba7f5494e9a652972fb7bea
Eliah Kagan <degeneracypressure@gmail.com>
2018-09-10 05:23:18 -0400

    implemented ListNode and about to test

--- ListNode.h
+++ ListNode.h
@@ -1,6 +1,36 @@
 #ifndef HAVE_POOL_LISTNODE_H_
 #define HAVE_POOL_LISTNODE_H_
 
+#include <initializer_list>
+#include <utility>
+#include "Pool.h"
 
+template<typename T>
+struct ListNode {
+    ListNode() : key{}, next{} { }
+
+    ListNode(const T& _key, ListNode* const _next) : key{_key}, next{_next} { }
+
+    ListNode(T&& _key, ListNode* const _next) : key{std::forward<T>(_key)},
+                                                next{_next} { }
+
+    T key;
+    ListNode* next;
+};
+
+template<typename T>
+ListNode<T>* make_list(Pool<ListNode<T>>& pool,
+                       const std::initializer_list<T> ilist)
+{
+    ListNode<T> sentinel {};
+
+    auto cur = &sentinel;
+    for (const auto& x : ilist) {
+        cur->next = pool(x, nullptr);
+        cur = cur->next;
+    }
+
+    return sentinel.next;
+}
 
 #endif // ! HAVE_POOL_LISTNODE_H_
--- test.cpp
+++ test.cpp
@@ -5,12 +5,16 @@
 
 int main()
 {
-    Pool<int> pool;
+    Pool<int> pi;
 
     std::vector<int*> a;
     for (auto i = 5; i != 0; --i)
-        a.push_back(pool(i));
+        a.push_back(pi(i));
 
     for (const auto x : a)
         std::cout << static_cast<const void*>(x) << ": " << *x << '\n';
+
+    Pool<ListNode<int>> pl;
+
+    auto head = make_list(pl, {10, 20, 30, 40, 50});
 }
