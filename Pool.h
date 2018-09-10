64dddef8bd7eaa3c1583ba57880539ca19ea3c1a
Eliah Kagan <degeneracypressure@gmail.com>
2018-09-09 20:23:48 -0400

    basic Pool implementation

--- Pool.h
+++ Pool.h
@@ -1,9 +1,21 @@
 #ifndef POOL_POOL_H_
 #define POOL_POOL_H_
 
+#include <deque>
+#include <utility>
+
 template<typename T>
 class Pool {
+public:
+    template<typename... Args>
+    T* operator()(Args&&... args)
+    {
+        objects_.emplace_back(std::forward<Args>(args)...);
+        return &objects_.back();
+    }
 
+private:
+    std::deque<T> objects_;
 };
 
 #endif // ! POOL_POOL_H_
