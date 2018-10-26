4968075ae23ce3a7d5cdea79613d7e2c785ae6dd
Eliah Kagan <degeneracypressure@gmail.com>
2018-10-26 17:28:56 -0400

    test with another tree

--- TreeNode-test.cpp
+++ TreeNode-test.cpp
@@ -14,26 +14,55 @@ namespace {
     {
         Pool<TreeNode<int>> p;
 
-        auto root = p(10, p(20, p(40), p(50)),
-                          p(30, p(60), p(70)));
+        auto r = p(10, p(20, p(40), p(50)),
+                       p(30, p(60), p(70)));
 
-        preorder_rec(root, [](auto& x) { ++x; });
-        inorder_rec(root, [](auto& x) { x += 2; });
-        postorder_rec(root, [](auto& x) { x += 4; });
+        preorder_rec(r, [](auto& x) { ++x; });
+        inorder_rec(r, [](auto& x) { x += 2; });
+        postorder_rec(r, [](auto& x) { x += 4; });
 
-        print_preorder_rec(root);
-        print_inorder_rec(root);
-        print_postorder_rec(root);
+        print_preorder_rec(r);
+        print_inorder_rec(r);
+        print_postorder_rec(r);
 
         std::cout << '\n';
 
-        preorder_iter(root, [](auto& x) { --x; });
-        inorder_iter(root, [](auto& x) { x -= 2; });
-        postorder_iter(root, [](auto& x) { x -= 4; });
+        preorder_iter(r, [](auto& x) { --x; });
+        inorder_iter(r, [](auto& x) { x -= 2; });
+        postorder_iter(r, [](auto& x) { x -= 4; });
 
-        print_preorder_iter(root);
-        print_inorder_iter(root);
-        print_postorder_iter(root);
+        print_preorder_iter(r);
+        print_inorder_iter(r);
+        print_postorder_iter(r);
+
+        std::cout << '\n';
+
+        Pool<TreeNode<char>> q;
+
+        auto s = q('a', q('b', q('d'), // first tree example from TAOCPv1 s2.3.1
+                               nullptr),
+                        q('c', q('e', nullptr,
+                                      q('g')),
+                               q('f', q('h'),
+                                      q('j'))));
+
+        preorder_rec(s, [](auto& x) { x -= 24; });
+        inorder_rec(s, [](auto& x) { x -= 6; });
+        postorder_rec(s, [](auto& x) { x -= 2; });
+
+        print_preorder_rec(s);
+        print_inorder_rec(s);
+        print_postorder_rec(s);
+
+        std::cout << '\n';
+
+        preorder_iter(s, [](auto& x) { x += 24; });
+        inorder_iter(s, [](auto& x) { x += 6; });
+        postorder_iter(s, [](auto& x) { x += 2; });
+
+        print_preorder_iter(s);
+        print_inorder_iter(s);
+        print_postorder_iter(s);
     }
 }
 
