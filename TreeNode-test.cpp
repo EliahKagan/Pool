13ab5747bf4007809f07d3a8eb0771f932109c41
Eliah Kagan <degeneracypressure@gmail.com>
2018-10-26 17:43:14 -0400

    tested with both left and right degenerate trees

--- TreeNode-test.cpp
+++ TreeNode-test.cpp
@@ -37,6 +37,58 @@ namespace {
 
         std::cout << '\n';
 
+        auto r2 = p(1, nullptr,
+                       p(2, nullptr,
+                            p(3, nullptr,
+                                 p(4, nullptr,
+                                      p(5)))));
+
+        preorder_rec(r2, [](auto& x) { x *= 2; });
+        inorder_rec(r2, [](auto& x) { x *= 2; });
+        postorder_rec(r2, [](auto& x) { x *= 2; });
+
+        print_preorder_rec(r2);
+        print_inorder_rec(r2);
+        print_postorder_rec(r2);
+
+        std::cout << '\n';
+
+        preorder_iter(r2, [](auto& x) { x /= 2; });
+        inorder_iter(r2, [](auto& x) { x /= 2; });
+        postorder_iter(r2, [](auto& x) { x /= 2; });
+
+        print_preorder_iter(r2);
+        print_inorder_iter(r2);
+        print_postorder_iter(r2);
+
+        std::cout << '\n';
+
+        auto r3 = p(1, p(2, p(3, p(4, p(5),
+                                      nullptr),
+                              nullptr),
+                         nullptr),
+                    nullptr);
+
+        preorder_rec(r3, [](auto& x) { x *= 2; });
+        inorder_rec(r3, [](auto& x) { x *= 2; });
+        postorder_rec(r3, [](auto& x) { x *= 2; });
+
+        print_preorder_rec(r3);
+        print_inorder_rec(r3);
+        print_postorder_rec(r3);
+
+        std::cout << '\n';
+
+        preorder_iter(r3, [](auto& x) { x /= 2; });
+        inorder_iter(r3, [](auto& x) { x /= 2; });
+        postorder_iter(r3, [](auto& x) { x /= 2; });
+
+        print_preorder_iter(r3);
+        print_inorder_iter(r3);
+        print_postorder_iter(r3);
+
+        std::cout << '\n';
+
         Pool<TreeNode<char>> q;
 
         auto s = q('a', q('b', q('d'), // first tree example from TAOCPv1 s2.3.1
