445075a19c3a5a8ee4f8daeadf42a053f4e083a6
Eliah Kagan <degeneracypressure@gmail.com>
2018-10-26 03:11:11 -0400

    added a test for some of the intended exception behavior

--- test-RaiiPrinter.cpp
+++ test-RaiiPrinter.cpp
@@ -1,13 +1,14 @@
 #include "RaiiPrinter.h"
 
 #include <iostream>
+#include <stdexcept>
 #include <string>
 #include <string_view>
 
 namespace {
     using namespace std::literals;
 
-    void test()
+    void simple_test()
     {
         char bar[] {"bar"};
 
@@ -18,11 +19,42 @@ namespace {
         print("baz"s);
         print("quux"sv);
     }
+
+    void run_simple_test()
+    {
+        std::cout << "Starting the SIMPLE test...\n";
+        simple_test();
+        std::cout << "...SIMPLE test done.\n\n";
+    }
+
+    void exceptions_test()
+    {
+        char bar[] {"bar"};
+
+        try {
+            ek::RaiiPrinter print {std::cout, ", ", "((", "))\n"};
+            print("foo");
+            print(42);
+            print(bar);
+            print("baz"s);
+            throw std::runtime_error{"meaningless exception for testing"};
+            print("quux"sv);
+        }
+        catch (const std::runtime_error& e) {
+            std::cout << "Got exception: " << e.what() << '\n';
+        }
+    }
+
+    void run_exceptions_test()
+    {
+        std::cout << "Starting the EXCEPTIONS test...\n";
+        exceptions_test();
+        std::cout << "...EXCEPTIONS test done.\n\n";
+    }
 }
 
 int main()
 {
-    std::cout << "Starting the test...\n";
-    test();
-    std::cout << "...test done.\n";
+    run_simple_test();
+    run_exceptions_test();
 }
