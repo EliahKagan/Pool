22e9edbf675914d0f3d6fabe95c0061c1ed3681a
Eliah Kagan <degeneracypressure@gmail.com>
2018-10-26 17:58:26 -0400

    about to implement the helper function that actually performs iteratively implemented algorithmically recursive depth-first search

--- TreeNode.h
+++ TreeNode.h
@@ -85,6 +85,12 @@ namespace ek {
             }
         }
 
+        template<typename P, typename FPre, typename FIn, typename FPost>
+        void dfs_node_rec_iter(P root, FPre f_pre, FIn f_in, FPost f_post)
+        {
+            // FIXME: implement this
+        }
+
         template<typename F>
         class OfKey {
         public:
@@ -114,6 +120,13 @@ namespace ek {
                             const FIn f_in, const FPost f_post)
         {
             dfs_node_iter(root, OfKey{f_pre}, OfKey{f_in}, OfKey{f_post});
+        }
+
+        template<typename P, typename FPre, typename FIn, typename FPost>
+        inline void dfs_rec_iter(const P root, const FPre f_pre,
+                                 const FIn f_in, const FPost f_post)
+        {
+            dfs_node_rec_iter(root, OfKey{f_pre}, OfKey{f_in}, OfKey{f_post});
         }
 
         inline constexpr auto noop = [](const auto&...) noexcept { };
@@ -191,7 +204,41 @@ namespace ek {
         detail::dfs_iter(root, detail::noop, detail::noop, f);
     }
 
-    // TODO: {pre,in,post}order_rec_iter (like continuation-passing style)
+    template<typename T, typename F>
+    inline void preorder_rec_iter(const TreeNode<T>* const root, const F f)
+    {
+        detail::dfs_rec_iter(root, f, detail::noop, detail::noop);
+    }
+
+    template<typename T, typename F>
+    inline void preorder_rec_iter(TreeNode<T>* const root, const F f)
+    {
+        detail::dfs_rec_iter(root, f, detail::noop, detail::noop);
+    }
+
+    template<typename T, typename F>
+    inline void inorder_rec_iter(const TreeNode<T>* const root, const F f)
+    {
+        detail::dfs_rec_iter(root, detail::noop, f, detail::noop);
+    }
+
+    template<typename T, typename F>
+    inline void inorder_rec_iter(TreeNode<T>* const root, const F f)
+    {
+        detail::dfs_rec_iter(root, detail::noop, f, detail::noop);
+    }
+
+    template<typename T, typename F>
+    inline void postorder_rec_iter(const TreeNode<T>* const root, const F f)
+    {
+        detail::dfs_rec_iter(root, detail::noop, detail::noop, f);
+    }
+
+    template<typename T, typename F>
+    inline void postorder_rec_iter(TreeNode<T>* const root, const F f)
+    {
+        detail::dfs_rec_iter(root, detail::noop, detail::noop, f);
+    }
 
     template<typename T>
     void print_preorder_rec(const TreeNode<T>* const root)
@@ -233,6 +280,27 @@ namespace ek {
     {
         RaiiPrinter print;
         postorder_iter(root, std::ref(print));
+    }
+
+    template<typename T>
+    void print_preorder_rec_iter(const TreeNode<T>* const root)
+    {
+        RaiiPrinter print;
+        preorder_rec_iter(root, std::ref(print));
+    }
+
+    template<typename T>
+    void print_inorder_rec_iter(const TreeNode<T>* const root)
+    {
+        RaiiPrinter print;
+        inorder_rec_iter(root, std::ref(print));
+    }
+
+    template<typename T>
+    void print_postorder_rec_iter(const TreeNode<T>* const root)
+    {
+        RaiiPrinter print;
+        postorder_rec_iter(root, std::ref(print));
     }
 }
 
